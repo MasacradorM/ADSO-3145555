773dfd00233f78fb90fa4a14319a83fe45f5d4c4
Oscar Mauricio Muñoz Mejía <145808047+MasacradorM@users.noreply.github.com>
2026-04-28 07:39:48 -0500

    Add files via upload

---
+++ 03-execution/03-week/03-optional-activity/cafetin/cafetin/src/main/java/sena/cafetin/CafetinApplication.java
@@ -0,0 +1,13 @@
+package sena.cafetin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafetinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafetinApplication.class, args);
+	}
+
+}
---
+++ 03-execution/03-week/03-optional-activity/cafetin/cafetin/src/test/java/sena/cafetin/CafetinApplicationTests.java
@@ -0,0 +1,13 @@
+package sena.cafetin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafetinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
